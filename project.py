f87bbfe0e101166245370caf51833142933bd921
Vanshaj Kochar <vanshaj.kochar@gmail.com>
2019-11-19 21:48:57 -0500

    Update project.py
    Adding methods for Data augmentation and visualisation

--- project.py
+++ project.py
@@ -19,3 +19,154 @@ import csv
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+dict_characters = {1:'NEUTROPHIL',2:'EOSINOPHIL',3:'MONOCYTE',4:'LYMPHOCYTE'}
+dict_characters2 = {0:'Mononuclear',1:'Polynuclear'}
+# Note that the function below is adapted from https://github.com/Shenggan/BCCD_Dataset
+image = cv2.imread("../input/dataset-master/dataset-master/JPEGImages/BloodImage_00022.jpg")
+tree = ET.parse("../input/dataset-master/dataset-master/Annotations/BloodImage_00022.xml")
+for elem in tree.iter():
+    if 'object' in elem.tag or 'part' in elem.tag:
+        for attr in list(elem):
+            if 'name' in attr.tag:
+                name = attr.text
+            if 'bndbox' in attr.tag:
+                for dim in list(attr):
+                    if 'xmin' in dim.tag:
+                        xmin = int(round(float(dim.text)))
+                    if 'ymin' in dim.tag:
+                        ymin = int(round(float(dim.text)))
+                    if 'xmax' in dim.tag:
+                        xmax = int(round(float(dim.text)))
+                    if 'ymax' in dim.tag:
+                        ymax = int(round(float(dim.text)))
+                if name[0] == "R":
+                    cv2.rectangle(image, (xmin, ymin),
+                                (xmax, ymax), (0, 255, 0), 1)
+                    cv2.putText(image, name, (xmin + 10, ymin + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (0, 255, 0), 1)
+                if name[0] == "W":
+                    cv2.rectangle(image, (xmin, ymin),
+                                (xmax, ymax), (0, 0, 255), 1)
+                    cv2.putText(image, name, (xmin + 10, ymin + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (0, 0, 255), 1)
+                if name[0] == "P":
+                    cv2.rectangle(image, (xmin, ymin),
+                                (xmax, ymax), (255, 0, 0), 1)
+                    cv2.putText(image, name, (xmin + 10, ymin + 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * image.shape[0], (255, 0, 0), 1)
+plt.figure(figsize=(16,16))
+plt.imshow(image)
+plt.show()
+
+
+# Plot Image
+def plotImage(image_location):
+    image = cv2.imread(image_location)
+    plt.imshow(image)
+    return
+image_name = '../input/dataset2-master/dataset2-master/images/TRAIN/EOSINOPHIL/_0_207.jpeg'
+plt.figure(figsize=(16,16))
+plt.subplot(221)
+plt.title('Eosinophil')
+plt.axis('off') 
+plotImage(image_name)
+image_name = '../input/dataset2-master/dataset2-master/images/TRAIN/LYMPHOCYTE/_0_204.jpeg'
+plt.subplot(222)
+plt.title('Lymphocyte')
+plt.axis('off') 
+plotImage(image_name)
+image_name = '../input/dataset2-master/dataset2-master/images/TRAIN/MONOCYTE/_0_180.jpeg'
+plt.subplot(223)
+plt.title('Monocyte')
+plt.axis('off') 
+plotImage(image_name)
+plt.subplot(224)
+image_name = '../input/dataset2-master/dataset2-master/images/TRAIN/NEUTROPHIL/_0_292.jpeg'
+plt.title('Neutrophil')
+plt.axis('off') 
+plotImage(image_name)
+
+reader = csv.reader(open('../input/dataset2-master/dataset2-master/labels.csv'))
+# skip the header
+next(reader)
+X3 = []                     #Initialize List to store labels
+y3 = []
+for row in reader:
+    label = row[2]  #Category in labels.csv file
+    if len(label) > 0 and label.find(',') == -1: #Check if label exist and check ',' does not exist in the label
+        y3.append(label)    #Add label to the list y3
+y3 = np.asarray(y3)   #convert list to numpy array
+encoder = LabelEncoder()  #Use Label Encoder to convert String Labels to Integer Labels
+encoder.fit(y3)
+encoded_y = encoder.transform(y3)
+counts = np.bincount(encoded_y)
+print(counts)
+fig, ax = plt.subplots()
+plt.bar(list(range(5)), counts)
+ax.set_xticklabels(('', 'Basophil', 'Eosinophil', 'Lymphocyte', 'Monocyte', 'Neutrophil'))
+ax.set_ylabel('Counts')
+
+from tqdm import tqdm
+def get_data(folder):
+    """
+    Load the data and labels from the given folder.
+    """
+    X = []
+    y = []
+    z = []
+    for wbc_type in os.listdir(folder):    #Access the folder for training and Test by converting image-folder titles to list 
+        if not wbc_type.startswith('.'):
+            if wbc_type in ['NEUTROPHIL']:
+                label = 1
+                label2 = 1
+            elif wbc_type in ['EOSINOPHIL']:
+                label = 2
+                label2 = 1
+            elif wbc_type in ['MONOCYTE']:
+                label = 3  
+                label2 = 0
+            elif wbc_type in ['LYMPHOCYTE']:
+                label = 4 
+                label2 = 0
+            else:
+                label = 5
+                label2 = 0
+            for image_filename in tqdm(os.listdir(folder + wbc_type)): #Access the folder contianing the Images for a particular WBC
+                img_file = cv2.imread(folder + wbc_type + '/' + image_filename)
+                #print(img_file)
+                if img_file is not None:   #Check for noneType - represents the absence of a value, such as when a function call returns an unexpected result or fails entirely.
+                    img_file = scipy.misc.imresize(arr=img_file, size=(60, 80, 3)) #Resizing the image 60X80 3 - channel image ? Memory Issue later on - Divide by 255
+                    img_arr = np.asarray(img_file) #Convert list to num py array
+                    X.append(img_arr)
+                    y.append(label)
+                    z.append(label2)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    z = np.asarray(z)
+    return X,y,z
+X_train, y_train, z_train = get_data('../input/dataset2-master/dataset2-master/images/TRAIN/')
+X_test, y_test, z_test = get_data('../input/dataset2-master/dataset2-master/images/TEST/')
+
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+from keras.utils.np_utils import to_categorical
+y_trainHot = to_categorical(y_train, num_classes = 5)  #Default value float32/ int64 ?
+y_testHot = to_categorical(y_test, num_classes = 5)
+z_trainHot = to_categorical(z_train, num_classes = 2)
+z_testHot = to_categorical(z_test, num_classes = 2)
+print(dict_characters)
+print(dict_characters2)
+
+import seaborn as sns
+df = pd.DataFrame()
+#print (df)
+df["labels"]=y_train   # Creating 9957 rows X 1 column DataFrame with label header
+#print ("-------")
+#print (df["labels"])
+lab = df['labels']
+#print ("-------")
+#print (type(lab))
+dist = lab.value_counts()
+#print("---------")
+#print(dist)
+sns.countplot(lab)    # plot Histogram of categorical data
+print(dict_characters)
