2646363e5b32e42ed09715bb45bbdf8b364a05b2
Vanshaj Kochar <vanshaj.kochar@gmail.com>
2019-11-23 18:43:55 -0500

    Update project.py
    Adding learning funnction and test function

--- project.py
+++ project.py
@@ -170,3 +170,174 @@ dist = lab.value_counts()
 #print(dist)
 sns.countplot(lab)    # plot Histogram of categorical data
 print(dict_characters)
+
+def plotHistogram(a):
+    """
+    Plot histogram of RGB Pixel Intensities
+    """
+    plt.figure(figsize=(10,5))
+    plt.subplot(1,2,1)
+    plt.imshow(a)
+    plt.axis('off')
+    histo = plt.subplot(1,2,2)
+    histo.set_ylabel('Count')
+    histo.set_xlabel('Pixel Intensity')
+    n_bins = 30            # Set the no. of vertical columns in the histogram graph
+    plt.hist(a[:,:,0].flatten(), bins= n_bins, lw = 0, color='r', alpha=0.5);
+    plt.hist(a[:,:,1].flatten(), bins= n_bins, lw = 0, color='g', alpha=0.5);
+    plt.hist(a[:,:,2].flatten(), bins= n_bins, lw = 0, color='b', alpha=0.5);  # what is lw used for ?
+plotHistogram(X_train[1])
+
+X_train=np.array(X_train) 
+X_train=X_train/255.0 #RGB values are 8 bit each with range 0-255
+
+X_test=np.array(X_test)   #Divide by 255 to reduce the range to 0.0 to 1.0
+X_test=X_test/255.0
+
+plotHistogram(X_train[1])
+
+# Helper Functions  Learning Curves and Confusion Matrix
+
+from keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+class MetricsCheckpoint(Callback):
+    """Callback that saves metrics after each epoch"""
+    def __init__(self, savepath):
+        super(MetricsCheckpoint, self).__init__()
+        self.savepath = savepath
+        self.history = {}
+    def on_epoch_end(self, epoch, logs=None):
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        np.save(self.savepath, self.history)
+
+def plotKerasLearningCurve():
+    plt.figure(figsize=(10,5))
+    metrics = np.load('logs.npy')[()]
+    filt = ['acc'] # try to add 'loss' to see the loss learning curve
+    for k in filter(lambda x : np.any([kk in x for kk in filt]), metrics.keys()):
+        l = np.array(metrics[k])
+        plt.plot(l, c= 'r' if 'val' not in k else 'b', label='val' if 'val' in k else 'train')
+        x = np.argmin(l) if 'loss' in k else np.argmax(l)
+        y = l[x]
+        plt.scatter(x,y, lw=0, alpha=0.25, s=100, c='r' if 'val' not in k else 'b')
+        plt.text(x, y, '{} = {:.4f}'.format(x,y), size='15', color= 'r' if 'val' not in k else 'b')   
+    plt.legend(loc=4)
+    plt.axis([0, None, None, None]);
+    plt.grid()
+    plt.xlabel('Number of epochs')
+    plt.ylabel('Accuracy')
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize = (5,5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def plot_learning_curve(history):
+    plt.figure(figsize=(8,8))
+    plt.subplot(1,2,1)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('./accuracy_curve.png')
+    #plt.clf()
+    # summarize history for loss
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig('./loss_curve.png')
+    
+    #print(len(y_trainHot[0]))
+print(len(X_train))
+
+import keras
+dict_characters = {1:'NEUTROPHIL',2:'EOSINOPHIL',3:'MONOCYTE',4:'LYMPHOCYTE'}
+dict_characters2 = {0:'Mononuclear',1:'Polynuclear'}
+def runKerasCNNAugment(a,b,c,d,e):
+    batch_size = 128          #batch size defines the number of samples that will be propagated through the network
+    num_classes = len(b[0])   #Length of y_trainHot (no. of classes = 5)
+    epochs = 30
+#     img_rows, img_cols = a.shape[1],a.shape[2]
+    img_rows,img_cols=60,80
+    input_shape = (img_rows, img_cols, 3)
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape,strides=e))    # First convolution layer with 32 filters, 3X3 kernel size, activation function = relu, input = 60X80X3 RGB  
+    model.add(MaxPooling2D(pool_size=(2, 2))) 
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2))) 
+    model.add(Conv2D(64, (3, 3), activation='relu'))        # Second Convolution layer with 64 filters, 3X3 Kernel size, activation = relu, input = 58X78X32
+    model.add(MaxPooling2D(pool_size=(2, 2)))               # Max-Pool layer with stride 2 input, output - 28*38*64        
+    model.add(Dropout(0.25))                                # Drop 25% of input going in the flaten layer
+    model.add(Flatten())                                    # Flatten the tensor into a 1 dimensional vector 
+    model.add(Dense(128, activation='relu'))                # Input the 1-D vector of 51072 elements into nueral network of 128 nodes  
+    model.add(Dropout(0.5))                                 # Drop 50% of the output
+    model.add(Dense(num_classes, activation='softmax'))     # Input the 64 element vector into 5 element Dense layer
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),    
+                  metrics=['accuracy'])                     # Compile model with optimiser Adadelta, loss function = categorical_crossentropy
+    datagen = ImageDataGenerator(                          # Creating batches of tensor image with augmented settings
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+    history = model.fit_generator(datagen.flow(a,b, batch_size=32),    # Train the model
+                        steps_per_epoch=len(a) / 32, epochs=epochs, validation_data = [c, d],callbacks = [MetricsCheckpoint('logs')]) #steps_per_epoch = 9957/32, epochs = 30
+    score = model.evaluate(c,d, verbose=0)  # Evaluates the accuracy on the test data
+    print('\nKeras CNN #1C - accuracy:', score[1],'\n')
+    y_pred = model.predict(c)     # output Numpy array(s) of prediction
+    map_characters = dict_characters
+    print('\n', sklearn.metrics.classification_report(np.where(d > 0)[1], np.argmax(y_pred, axis=1), target_names=list(map_characters.values())), sep='')   # Print Table for Accuraccy, precsion, F1 socre and callback 
+    Y_pred_classes = np.argmax(y_pred,axis=1) 
+    Y_true = np.argmax(d,axis=1)
+    plotKerasLearningCurve()
+    plt.show()  
+    plot_learning_curve(history)
+    plt.show()
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
+    plot_confusion_matrix(confusion_mtx, classes = list(dict_characters.values())) 
+    plt.show()
+runKerasCNNAugment(X_train,y_trainHot,X_test,y_testHot,1)
+
+print(score)
+
+dict_characters = dict_characters2
+runKerasCNNAugment(X_train,z_trainHot,X_test,z_testHot,2)   
+dict_characters = {1:'NEUTROPHIL',2:'EOSINOPHIL',3:'MONOCYTE',4:'LYMPHOCYTE'}
+dict_characters2 = {0:'Mononuclear',1:'Polynuclear'}
